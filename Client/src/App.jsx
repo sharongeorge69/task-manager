3d0f0277b9fd4eabe2ae1e117c999975a38e1ff1
sharongeorge69 <georgesharon6969@gmail.com>
2025-11-05 21:40:19 +0530

    Added UserAvatar and NotificationPanel components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,6 +9,13 @@ import { Toaster } from "sonner";
 import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import { useRef, Fragment } from "react";
+import { useDispatch } from "react-redux";
+import { setOpenSidebar } from "./redux/slices/authSlice";
+import clsx from "clsx";
+import { Transition } from "@headlessui/react";
+import { IoClose } from "react-icons/io5";
+
 
 function Layout() {
 	const { user } = useSelector((state) => state.auth);
@@ -18,7 +25,7 @@ function Layout() {
 			<div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
 				<Sidebar />
 			</div>
-			{/* <MobileSidebar/> */}
+			<MobileSidebar/>
 			<div className="flex-1 overflow-y-auto">
 				<Navbar />
 				<div className="p-4 2xl:px-10">
@@ -31,6 +38,70 @@ function Layout() {
 		<Navigate to="/log-in" state={{ from: location }} replace />
 	);
 }
+
+
+const MobileSidebar = () => {
+	const { isSidebarOpen } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+	const sidebarRef = useRef(null);
+
+	const closeSidebar = () => {
+		dispatch(setOpenSidebar(false));
+	};
+
+	return (
+		<Transition show={!!isSidebarOpen}>
+			{/* Backdrop */}
+			<div
+				className="fixed inset-0 z-50 md:hidden"
+				onClick={closeSidebar}
+			>
+				{/* Animated Panel */}
+				<Transition.Child
+					enter="transition-transform duration-300 ease-out"
+					enterFrom="-translate-x-full"
+					enterTo="translate-x-0"
+					leave="transition-transform duration-300 ease-in"
+					leaveFrom="translate-x-0"
+					leaveTo="-translate-x-full"
+				>
+					<div
+						ref={sidebarRef}
+						className="absolute left-0 top-0 h-full w-3/4 bg-white shadow-xl"
+						onClick={(e) => e.stopPropagation()}
+					>
+						{/* Close Button */}
+						<div className="flex justify-end p-4">
+							<button
+								onClick={closeSidebar}
+								className="rounded-full p-2 hover:bg-gray-100 transition"
+							>
+								<IoClose size={24} className="text-gray-600" />
+							</button>
+						</div>
+
+						{/* Sidebar Content */}
+						<div className="px-4 -mt-6">
+							<Sidebar />
+						</div>
+					</div>
+				</Transition.Child>
+
+				{/* Optional: Fade backdrop */}
+				<Transition.Child
+					enter="transition-opacity duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="transition-opacity duration-300"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="absolute inset-0 bg-black/50" />
+				</Transition.Child>
+			</div>
+		</Transition>
+	);
+};
 
 const App = () => {
 	return (
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import { MdOutlineSearch } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { setOpenSidebar } from "../redux/slices/authSlice";
 import UserAvatar from "./UserAvatar";
+import { FiMenu } from "react-icons/fi";
+import Notificationpanel from "./Notificationpanel";
 
 const Navbar = () => {
 	const { user } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	return (
 		<div className="flex justify-between items-center bg-white px-4 py-3 2xl:py-4 sticky z-10 top-0">
 			<div className="flex gap-4">
@@ -14,34 +16,22 @@ const Navbar = () => {
 					className="text-2xl text-gray-500 block md:hidden"
 					onClick={() => dispatch(setOpenSidebar(true))}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth={2}
-							d="M4 6h16M4 12h16M4 18h16"
-						/>
-					</svg>
+					<FiMenu />
 				</button>
-                <div className="w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]">
-                    <MdOutlineSearch className="text-gray-500 text-xl" />
-                    <input type="text" 
-                    placeholder="Search"
-                    className="flex-1 outline-none bg-transparent placeholder:text-gray-500 text-gray-800"
-                    />
-                </div>
+				<div className="w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]">
+					<MdOutlineSearch className="text-gray-500 text-xl" />
+					<input
+						type="text"
+						placeholder="Search"
+						className="flex-1 outline-none bg-transparent placeholder:text-gray-500 text-gray-800"
+					/>
+				</div>
 			</div>
-            <div className="flex gap-2 items-center">
-                {/* <NotificationPanel /> */}
-                <UserAvatar/>
-            </div>
+			<div className="flex gap-2 items-center">
+				<Notificationpanel />
+				<UserAvatar />
+			</div>
 		</div>
 	);
-}
+};
 export default Navbar;
